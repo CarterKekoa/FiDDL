370b7b98dc196a5c4f06014e5d5c4bc236eb45f9
CarterMooring <carterkekoa@gmail.com>
2020-11-01 12:11:50 -0800

    Group

--- app.py
+++ app.py
@@ -2,12 +2,12 @@
 
 from flask import Flask, render_template     #import flask libries, render_template is so we can make front end
 
-app = Flask(__name__)       #call flask constuctor from object #__name__ references this file
+app = Flask(__name__)                        #call flask constuctor from object #__name__ references this file
 
 #define url with a route
-@app.route('/')                                               #@ is a decorator, flask uses this to define its urls
+@app.route('/')                                                #@ is a decorator, flask uses this to define its urls
 def index():
-    return render_template('index.html')    #must be in directory (folder) names templates, grabs file form there
+    return render_template('index.html')                       #must be in directory (folder) names templates, grabs file form there
 
 @app.route('/home/users/<string:name>/posts/<int:id>')         #this runs the same function below, but now with url 'localhost:5000/home', so both pages show this now
 def hello(name, id):                                           #this code runs when we get to the above url, now 'name' references '<string:name>' so we can grab what is in the url there
